ebfafc407dcd0f83f7b468086db025993ec9f003
Kiki <caiquelsobral@gmail.com>
2023-10-05 20:21:45 -0300

    Chore: Remove unused interfaces

--- src/interfaces/Pokemon.ts
+++ src/interfaces/Pokemon.ts
@@ -1,10 +1,3 @@
-import { ImageProps } from 'react-native';
-
-export interface PokemonData {
-  name: number;
-  url: string;
-}
-
 export interface PokemonInterface {
   id: number;
   name: string;
@@ -13,9 +6,5 @@ export interface PokemonInterface {
   sprite: string;
   captureRate: number;
   evolutionChainId: number;
-  types: Array<PokemonTypes>;
-}
-
-interface PokemonTypes {
-  name: string;
+  types: Array<string>;
 }
