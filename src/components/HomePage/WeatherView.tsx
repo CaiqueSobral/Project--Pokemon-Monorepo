be01784720da92fbda57efc447a44c3051636f57
Kiki <caiquelsobral@gmail.com>
2023-10-10 00:37:52 -0300

    Feat: Add weather view

---
+++ src/components/HomePage/WeatherView.tsx
@@ -0,0 +1,52 @@
+import { FONTSTART2P } from '../../data/constants';
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import PrimaryText from '../Custom/PrimaryText';
+
+export default function WeatherView() {
+  return (
+    <View className="relative w-[100%] h-[30%] border-4 border-black mb-10 items-center justify-center">
+      <View className="h-full w-full bg-white p-2">
+        <View className="flex-1 flex-row">
+          <View className="flex-1 justify-center items-center flex-col">
+            <View className="flex-1 pt-[5] justify-center items-center flex-row">
+              <PrimaryText classname={'text-5xl'} text={'27'} />
+              <PrimaryText classname={'text-2xl'} text={'°C'} />
+            </View>
+          </View>
+          <View className="flex-1">
+            <Image
+              source={{
+                uri: 'https://piskel-imgstore-b.appspot.com/img/710d6bd7-671d-11ee-8b06-176710959793.gif',
+              }}
+              resizeMode="contain"
+              className="h-full w-full"
+            />
+          </View>
+        </View>
+        <View className="w-full items-center justify-center">
+          <View className="w-full mt-2 items-center flex-row justify-center">
+            <Image
+              source={{
+                uri: 'https://cdn-icons-png.flaticon.com/512/64/64113.png',
+              }}
+              resizeMode="contain"
+              className="h-3 w-3 mr-1"
+            />
+            <PrimaryText classname={'text-xs pt-[5]'} text={'Itaqua, Monday'} />
+          </View>
+        </View>
+      </View>
+      <View className="absolute h-full w-full -z-50">
+        <View className="absolute bg-black h-[106%] w-[100%] -z-50 -top-[3%]"></View>
+        <View className="absolute bg-black h-[100%] w-[104%] -z-50 -left-[2%]"></View>
+      </View>
+      <PrimaryText
+        classname={
+          'absolute text-sm -top-3 text-center left-4 bg-white z-50 px-2'
+        }
+        text="Weather"
+      />
+    </View>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,13 +4,17 @@ import { NavigationScreensProps } from '../routes/HomeNavigator';
 import React, { useContext } from 'react';
 import { Pressable, Text, View } from 'react-native';
 import { WeatherContext } from '../data/context/weatherContext';
+import HomeHeader from '../components/HomePage/HomeHeader';
+import WeatherView from '../components/HomePage/WeatherView';
 
 export default function HomePage({ navigation }: NavigationScreensProps) {
   const weatherContext = useContext(WeatherContext);
   return (
-    <SafeAreaView className="flex-1 mt-2">
+    <SafeAreaView className="flex-1 mt-2 bg-white">
       <Header title="" openDrawer={navigation.openDrawer} />
-      <View className="flex-1 justify-center items-center">
+      <View className="flex-1 m-4 items-center">
+        <HomeHeader />
+        <WeatherView />
         <Text>{`Welcome from`}</Text>
         <Pressable onPress={navigation.openDrawer}>
           <Text>Open Drawer</Text>
