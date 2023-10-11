e86676ebc941a9042ac2b23f14d8eb9f952f2545
Kiki <caiquelsobral@gmail.com>
2023-10-11 20:43:55 -0300

    Feat: Add action buttons to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,8 @@ import { View } from 'react-native';
 import { WeatherContext } from '../data/context/weatherContext';
 import HomeHeader from '../components/HomePage/HomeHeader';
 import WeatherView from '../components/HomePage/WeatherView';
+import PrimaryButton from '../components/Custom/PrimaryButton';
+import ContainerWithTitle from '../components/Custom/ContainerWithTitle';
 
 export default function HomePage({ navigation }: NavigationScreensProps) {
   const weatherContext = useContext(WeatherContext);
@@ -13,6 +15,8 @@ export default function HomePage({ navigation }: NavigationScreensProps) {
   const dayOfWeek = new Date()
     .toLocaleString('en-us', { weekday: 'long' })
     .split(',')[0];
+
+  const buttons = ['Pokédex', 'Travel', 'Backpack', 'Your Pokémons', 'Profile'];
   return (
     <SafeAreaView className="flex-1 mt-2 bg-white">
       <Header title="" openDrawer={navigation.openDrawer} />
@@ -24,6 +28,15 @@ export default function HomePage({ navigation }: NavigationScreensProps) {
           day={dayOfWeek}
           icon={{ uri: weatherContext.currentWeather.weather.condition.icon }}
         />
+        <View className="flex-1 w-full h-full justify-center items-center my-4">
+          {buttons.map((button) => {
+            return (
+              <View className="flex-1 w-full items-center justify-center">
+                <PrimaryButton text={button} />
+              </View>
+            );
+          })}
+        </View>
       </View>
     </SafeAreaView>
   );
