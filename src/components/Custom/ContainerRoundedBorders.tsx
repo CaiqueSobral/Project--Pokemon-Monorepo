c7a0883b42f1290eb97be7c33921cc216e6d33bc
Kiki <caiquelsobral@gmail.com>
2023-10-13 21:11:53 -0300

    Refac: Refactor containers

--- src/components/Custom/ContainerRoundedBorders.tsx
+++ src/components/Custom/ContainerRoundedBorders.tsx
@@ -4,13 +4,20 @@ import PrimaryText from './PrimaryText';
 import Custom8BitRoundedBorders from './Custom8BitRoundedBorders';
 
 type Props = {
+  title?: string;
   children: ReactNode;
 };
-export default function ContainerWithTitle(props: Props) {
+export default function ContainerWithRoundedBorders(props: Props) {
   return (
     <View className="relative w-[100%] h-[100%] border-4 border-black items-center justify-center">
       {props.children}
       <Custom8BitRoundedBorders />
+      {props.title && (
+        <PrimaryText
+          classname={'absolute text-sm -top-3 text-center left-4 bg-white px-2'}
+          text={props.title}
+        />
+      )}
     </View>
   );
 }
