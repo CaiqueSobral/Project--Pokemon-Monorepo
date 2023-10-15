80b05f2e148ce1118cb158effac5c5425e05b043
Kiki <caiquelsobral@gmail.com>
2023-10-14 21:27:25 -0300

    Assets: Add types colors

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -17,3 +17,24 @@ export const weatherIcons = {
   light_snow: 'https://media.giphy.com/media/Q77zarHqN5ylVsH4OH/giphy.gif',
   torrential_rain: 'https://media.giphy.com/media/gjHPrvAHw7USQKtVyy/giphy.gif',
 };
+
+const pokemonTypesColors = {
+  normal: '#A8A77A',
+  fire: '#EE8130',
+  water: '#6390F0',
+  electric: '#F7D02C',
+  grass: '#7AC74C',
+  ice: '#96D9D6',
+  fighting: '#C22E28',
+  poison: '#A33EA1',
+  ground: '#E2BF65',
+  flying: '#A98FF3',
+  psychic: '#F95587',
+  bug: '#A6B91A',
+  rock: '#B6A136',
+  ghost: '#735797',
+  dragon: '#6F35FC',
+  dark: '#705746',
+  steel: '#B7B7CE',
+  fairy: '#D685AD',
+};
