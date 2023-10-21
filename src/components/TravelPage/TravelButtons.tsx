06db965cb57914ca119c3b885a6048177c2f8fd5
Kiki <caiquelsobral@gmail.com>
2023-10-20 22:52:49 -0300

    Feat: Componentizate travel buttons

---
+++ src/components/TravelPage/TravelButtons.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View } from 'react-native';
+import PrimaryButton from '../Custom/PrimaryButton';
+import { ICarouselInstance } from 'react-native-reanimated-carousel';
+
+type Props = {
+  carouselRef: React.RefObject<ICarouselInstance>;
+};
+
+export default function TravelButtons(props: Props) {
+  return (
+    <View className="flex-1 w-full items-center my-2 space-y">
+      <View className="flex-1 w-full items-center justify-center flex-row space-x-4">
+        <View className="flex-1 w-full items-center">
+          <PrimaryButton
+            text="< Prev."
+            onPress={props.carouselRef.current?.prev}
+          />
+        </View>
+        <View className="flex-1 w-full items-center">
+          <PrimaryButton
+            text="Next >"
+            onPress={props.carouselRef.current?.next}
+          />
+        </View>
+      </View>
+      <View className="flex-1 w-full justify-center items-center">
+        <PrimaryButton text="Go" />
+      </View>
+    </View>
+  );
+}
