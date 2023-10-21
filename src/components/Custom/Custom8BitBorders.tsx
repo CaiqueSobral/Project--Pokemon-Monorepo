07ee3cf90f3f5e718ecc45aaa81ff7a22e76ded0
Kiki <caiquelsobral@gmail.com>
2023-10-20 22:52:16 -0300

    Refac: Refactor 8 bit borders

--- src/components/Custom/Custom8BitBorders.tsx
+++ src/components/Custom/Custom8BitBorders.tsx
@@ -4,10 +4,10 @@ import { View } from 'react-native';
 export default function Custom8BitBorders() {
   return (
     <View className="absolute w-full h-full -z-50">
-      <View className="absolute w-full h-[4] bg-black -z-50 top-[100%]"></View>
-      <View className="absolute w-full h-[4] bg-black -z-50 bottom-[100%]"></View>
-      <View className="absolute w-[4] h-full bg-black -z-50 right-[100%]"></View>
-      <View className="absolute w-[4] h-full bg-black -z-50 left-[100%]"></View>
+      <View className="absolute w-full h-[4] bg-black top-full"></View>
+      <View className="absolute w-full h-[4] bg-black bottom-full"></View>
+      <View className="absolute w-[4] h-full bg-black right-full"></View>
+      <View className="absolute w-[4] h-full bg-black left-full"></View>
     </View>
   );
 }
