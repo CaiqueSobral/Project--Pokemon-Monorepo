3617e3feb554599c7be4103693a3156d22a7a043
Kiki <kiki@ibm.com>
2023-10-30 16:59:06 -0300

    Feat: Add animation for trainer and pokemon when pokemon is found

--- src/components/HuntingPage/FoundPokemonComponent.tsx
+++ src/components/HuntingPage/FoundPokemonComponent.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Image, View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSpring,
+} from 'react-native-reanimated';
 
 type Props = {
   sprite: {
@@ -7,25 +13,66 @@ type Props = {
     height: number;
     width: number;
   };
+  size: number;
 };
 export default function PokemonPicture(props: Props) {
+  const offsetPokemon = useSharedValue(0);
+  const offsetTrainer = useSharedValue(0);
+
+  const animatePokemonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offsetPokemon.value }],
+    };
+  });
+  const animateTrainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: -offsetTrainer.value }],
+    };
+  });
+
+  React.useEffect(() => {
+    offsetPokemon.value = withDelay(
+      150,
+      withSpring(props.size / 3 - 16, { mass: 1, damping: 50 }),
+    );
+    offsetTrainer.value = withDelay(
+      150,
+      withSpring(props.size / 2 - 8, { mass: 1, damping: 50 }),
+    );
+  });
+
   return (
-    <View className="h-[75%] w-full">
-      <View className="flex-1 items-center">
-        <View className="h-full w-full absolute self-center justify-self-end bottom-[5%]">
-          <Image
-            source={require('../../../assets/images/pokemon/field.png')}
-            className="w-full h-full opacity-85"
-          />
+    <>
+      <Animated.View
+        style={[animatePokemonStyle]}
+        className="h-1/2 w-2/3 absolute left-2 top-4"
+      >
+        <View className="flex-1 items-center">
+          <View className="h-full w-full absolute self-center justify-self-end">
+            <Image
+              source={require('../../../assets/images/pokemon/field.png')}
+              className="w-full h-full opacity-85"
+            />
+          </View>
+          <View className="flex absolute items-center justify-center bottom-[15%]">
+            <Image
+              height={props.sprite.height * 1.5}
+              width={props.sprite.width * 1.5}
+              source={{ uri: props.sprite.uri }}
+            />
+          </View>
         </View>
-        <View className="flex absolute items-center justify-center bottom-[15%]">
-          <Image
-            height={props.sprite.height * 2}
-            width={props.sprite.width * 2}
-            source={{ uri: props.sprite.uri }}
-          />
-        </View>
-      </View>
-    </View>
+      </Animated.View>
+      <Animated.View
+        style={[animateTrainerStyle]}
+        className="h-2/3 w-2/3 absolute -bottom-[2] right-0"
+      >
+        <Image
+          source={require('../../../assets/images/trainer-back.png')}
+          resizeMode="contain"
+          className="h-full w-full"
+        />
+      </Animated.View>
+    </>
   );
 }
