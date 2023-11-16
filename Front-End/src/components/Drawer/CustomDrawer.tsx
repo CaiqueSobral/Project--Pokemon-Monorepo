8a9366441feb9ec2aebe6f829dccbc1935a0b8a8
Kiki <caiquelsobral@gmail.com>
2023-11-15 23:47:43 -0300

    Feat: Add a custom drawer

---
+++ Front-End/src/components/Drawer/CustomDrawer.tsx
@@ -0,0 +1,49 @@
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import React from 'react';
+import { View } from 'react-native';
+import PrimaryText from '../Custom/PrimaryText';
+import { Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { pokeballs } from '../../data/constants';
+
+export default function CustomDrawer(props: any) {
+  return (
+    <SafeAreaView className="flex-1 px-3">
+      <View className="h-1/5 w-full border-b-2">
+        <View className="h-3/4 aspect-square">
+          <Image
+            source={require('../../../assets/adaptive-icon.png')}
+            resizeMode="contain"
+            className="h-full w-full"
+          />
+        </View>
+        <View className="w-full flex-row pt-2">
+          <View className="flex-1 items-start justify-end">
+            <PrimaryText text={`₽ 500`} />
+          </View>
+          <View className="flex-1 flex-row justify-end">
+            <View className="h-[32] w-[32]">
+              <Image
+                source={{ uri: pokeballs[0].sprite }}
+                resizeMode="contain"
+                className="h-full w-full"
+              />
+            </View>
+            <View className="flex justify-end">
+              <PrimaryText text={`12`} />
+            </View>
+          </View>
+        </View>
+      </View>
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+      <View className="mb-4 h-8 justify-center">
+        <PrimaryText text="Log out" classname="pt-[8]" />
+      </View>
+    </SafeAreaView>
+  );
+}
