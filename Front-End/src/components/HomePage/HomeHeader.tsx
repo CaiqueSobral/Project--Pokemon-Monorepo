9923271d33ee93686e8f3d1c2af997848f533ebb
Kiki <caiquelsobral@gmail.com>
2023-11-25 12:20:42 -0300

    Feat: Add user name in header

--- Front-End/src/components/HomePage/HomeHeader.tsx
+++ Front-End/src/components/HomePage/HomeHeader.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Image, View } from 'react-native';
 import PrimaryText from '../Custom/PrimaryText';
+import { UserContext } from '../../data/context/userContext';
 
 export default function HomeHeader() {
+  const { user } = useContext(UserContext);
   return (
     <View className="flex-1 w-full flex-row items-center mt-4 mb-4">
       <View className="flex-1 justify-center items-start pt-[10]">
-        <PrimaryText classname={'text-xl'} text="Welcome back, User" />
+        <PrimaryText
+          classname={'text-xl'}
+          text={`Welcome back, ${user.name}`}
+        />
       </View>
       <View className="flex h-20 w-20 justify-center items-center rounded-full border border-black">
         <Image
